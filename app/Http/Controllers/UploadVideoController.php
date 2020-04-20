d104027755dd6e4bd02c6092c20e6222abd5e6ee
Kevin Hou <kevin.rd@howdesign.com.tw>
2020-04-20 16:54:38 +0800

    lesson-18-setting-up-jobs-and-queue-workers

--- app/Http/Controllers/UploadVideoController.php
+++ app/Http/Controllers/UploadVideoController.php
@@ -4,6 +4,7 @@ namespace Laratube\Http\Controllers;
 
 use Laratube\Channel;
 use Illuminate\Http\Request;
+use Laratube\Jobs\Videos\ConvertForStreaming;
 
 class UploadVideoController extends Controller
 {
@@ -16,9 +17,17 @@ class UploadVideoController extends Controller
 
     public function store(Channel $channel)
     {
-        return $channel->videos()->create([
+        // return $channel->videos()->create([
+        //     'title' => request()->title,
+        //     'path' => request()->video->store("channels/{$channel->id}")
+        // ]);
+        $video = $channel->videos()->create([
             'title' => request()->title,
             'path' => request()->video->store("channels/{$channel->id}")
         ]);
+
+        $this->dispatch(new ConvertForStreaming($video));
+
+        return $video;
     }
 }
