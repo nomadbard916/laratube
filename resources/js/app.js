79cb0f7feb943ee9d688d15cd337393dc6db6feb
Kevin Hou <kevin.rd@howdesign.com.tw>
2020-04-21 17:02:53 +0800

    lesson-23-display-video-player

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue')
 
+Vue.config.ignoredElements = [
+    'video-js'
+]
 require('./components/subscribe-button')
 require('./components/channel-upload')
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,6 +19,7 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @yield('styles')
     </head>
 
     <body>
@@ -99,6 +100,8 @@
                     }
                 }
         </script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('scripts')
     </body>
 
 </html>
